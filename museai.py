55d4691f8aa10c884b5cf1cdd38622b3eb1d7b8d
epsevilleno <ejprince.sevilleno@wvsu.edu.ph>
2024-05-26 12:51:44 +0800

    made comments for comprehension in the museai.py

--- museai.py
+++ museai.py
@@ -4,10 +4,12 @@ import openai
 from openai import AsyncOpenAI
 from openai import OpenAI
 
+#API Key was typed into streamlit during the integration process
 client = AsyncOpenAI (
     api_key=st.secrets["API_key"]
 )
 
+#function for GPT to generate response
 async def generate_response(question, context):
     model = "gpt-3.5-turbo"
 
@@ -38,29 +40,40 @@ async def app():
     #User inputs to process
     feeling_type = st.text_input("What do you feel right now? (e.g., feeling determined)")   
     
+    #The following would only appear if feeling_type was filled in
     if feeling_type:
+        
+        #creating a response based on the user's feeling (based on the input in feeling_type)
         museai_context = f'Provide a one-sentence supportive message based on {feeling_type} and say to proceed to next section.'
         museai_question = "How should I respond to this?"
         museai_response = await generate_response(museai_question, museai_context)
         
+        #will write the response
         st.write(museai_response)
-                     
+        
+        #Will accept the type of genre the user selects             
         genre_type = st.selectbox("What genre of music would you like to find?", ['Pop', 'Rock', 'Country', 'Classical', 'Jazz', 'Hip hop', 'Other'])
+        
+        #a field that would appear if the user selects 'Other'
         if genre_type == 'Other':
             other_genre = st.text_input("Please specify what genre you would like.")
             if other_genre:
                 genre_type = other_genre
         
+        # Will accept User's the preferred language
         language_type = st.text_input("What language do you prefer?")
         
+        #an if-statement that would make the components below appear if language_type is filled in
         if language_type:
             
             st.write("Make your choices more tailored by proceeding to the next section.")
-        
+            
+            #will accept user's preferences        
             tempo_type = st.selectbox("How fast would you like the song/s?", ['slow', 'moderate', 'fast', 'any'])
             activity_type = st.text_input("What are you doing right now? (e.g., studying or working-out )")   
             music_amount = st.text_input("How many songs would you like to generate? (e.g., 3 or three)") 
             
+            #a button that would trigger the generate response function would appear if the music_amount is filled in
             if music_amount:
                 
                 st.write("Great! Now click the button below.")
